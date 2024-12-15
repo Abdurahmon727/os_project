c689dac8719b71cd72851c8b0ad409b9cee8a726
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-12-15 11:53:09 +0500

    feat: regions

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -1,4 +1,20 @@
 abstract class Constants {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
   static const String defaultErrorMessage = 'Something went wrong 😞';
+  List<String> uzbekistanRegions = [
+    "Tashkent",
+    "Andijan",
+    "Bukhara",
+    "Fergana",
+    "Jizzakh",
+    "Kashkadarya",
+    "Khorezm",
+    "Namangan",
+    "Navoi",
+    "Samarkand",
+    "Sirdarya",
+    "Surkhandarya",
+    "Tashkent",
+    "Karakalpakstan",
+  ];
 }
