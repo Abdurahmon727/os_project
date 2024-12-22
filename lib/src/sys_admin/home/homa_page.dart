cf11136a3f3221058a2fece930d85a5146dbd3a5
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-12-22 21:00:11 +0500

    feat: sys admin home page ui

--- lib/src/sys_admin/home/homa_page.dart
+++ lib/src/sys_admin/home/homa_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+import 'package:os_project/core/enums/formz_status.dart';
+
+import '../../../assets/assets.dart';
+import '../../../core/widget/custom_app_bar.dart';
+import '../../client/home/widget/post_preview.dart';
+import 'bloc/sys_admin_home_bloc.dart';
 
 class SysAdminHomePage extends StatelessWidget {
   const SysAdminHomePage({super.key});
@@ -6,10 +14,34 @@ class SysAdminHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('sys admin home page'),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: CustomAppBar(title: 'Sys admin home page'),
+      ),
+      body: BlocBuilder<SysAdminHomeBloc, SysAdminHomeState>(
+        builder: (context, state) {
+          if (state.status.isLoading) {
+            return Center(
+              child: Lottie.asset(AppAnimations.loadingAnimation),
+            );
+          }
+
+          return GridView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              childAspectRatio: 1.75,
+            ),
+            itemCount: 20,
+            itemBuilder: (context, index) => WPostPreview(
+              onTap: () {},
+              image: 'https://picsum.photos/800/500',
+              title: 'room new $index',
+            ),
+          );
+        },
       ),
     );
   }
