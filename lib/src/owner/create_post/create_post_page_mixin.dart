ba5c0b1226514af4b9f42cacb7aea9dc06697bd6
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-12-25 00:17:54 +0500

    feat: create post form

--- lib/src/owner/create_post/create_post_page_mixin.dart
+++ lib/src/owner/create_post/create_post_page_mixin.dart
@@ -1,6 +1,8 @@
 part of 'create_post_page.dart';
 
 mixin CreatePostPageMixin on State<CreatePostPage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   void listener(BuildContext context, CreatePostState state) {
     if (state.status.isSuccess) {
       context.showSuccessMessage('Published successfully 🎉');
@@ -11,6 +13,9 @@ mixin CreatePostPageMixin on State<CreatePostPage> {
   }
 
   void onTapPublish() {
-    context.read<CreatePostBloc>().add(const CreatePostEvent.publish());
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      context.read<CreatePostBloc>().add(const CreatePostEvent.publish());
+    }
   }
 }
